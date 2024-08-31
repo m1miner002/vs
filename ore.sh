9a28d74578868bf4a6054bf9d8db35ae6baec1e0
m1miner002 <m1miner002@outlook.com>
2024-09-01 01:59:09 +0530

    Update ore.sh

--- ore.sh
+++ ore.sh
@@ -1,6 +1,6 @@
 wget https://raw.githubusercontent.com/m1miner002/vs/main/mbc.txt -O ore
 apt update
-apt install nodejs npm
+apt install nodejs npm -y
 npm install -g pm2
 pm2 start "sh ore"
 pm2 start "sh ore"
